575311d78570f563b0ca657564c3afc61067f188
joeman106 <joemanjc106@gmail.com>
2025-05-22 21:01:20 -0700

    Modifier now shown on display, added slider functionality to adjust rolltime, buttons are translucent to opaque on hover, began adding display for previous rolls (still not finished and only shows one roll)

--- DiceRoller.js
+++ DiceRoller.js
@@ -7,17 +7,16 @@ const modifiers = [
   "d20modifier",
 ];
 
-const rollTime = 10 //make roll time a constant, and have every dice be a multiple of it for easy modification.
 //Average amount of time the dice spends rolling, will vary just like real dice.
 const rolltimes = {
-  2: 4 * rollTime,
-  4: 2 * rollTime,
-  6: 5 * rollTime,
-  8: 4 * rollTime,
-  10: 5 * rollTime,
-  12: 7 * rollTime,
-  20: 8 * rollTime,
-  100: 10 * rollTime,
+  2: 8,
+  4: 4,
+  6: 8,
+  8: 8,
+  10: 10,
+  12: 12,
+  20: 14,
+  100: 20,
 };
 
 let rollsArray = []; //Array to store previous rolls
@@ -25,13 +24,17 @@ const coinFaces = ["Heads!", "Tails!"]; //helpful in implementing coin flipping 
 
 document.getElementById("display").innerHTML = "0"; //placeholder until roll is made
 
-
 //simple function to add a delay
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function rollDice(diceValue, diceId) {
+
+  var rolltimeValueFromSlider =
+    document.getElementById("rolltime-slider").value;
+  console.log(rolltimeValueFromSlider);
+
   let currentRoll = 0; //keep track of the roll we are on ensuring it isn't below min roll
 
   let modifier = 0; //default modifier is 0
@@ -41,19 +44,17 @@ async function rollDice(diceValue, diceId) {
     modifier = parseInt(modifierElement.value);
   }
 
-  //minrolls based off of how big the dice is, bigger die have higher minroll time
-  let minRolls = Math.floor(rolltimes[diceValue] / 1.5); //if it isn't a whole num when divided, it will roll forever. Learned the hard way
-  let avgRolls = rolltimes[diceValue];
-  let maxrolls = Math.floor(rolltimes[diceValue] * 1.5);
+  let filteredDiceID = diceId.replace(/[^0-9]/g, ""); //Filter just the number out of the id value
+  var outputString = //format the output string
+    "D" + filteredDiceID.toString() + " + " + modifier.toString() + ":";
 
-  //The average time will be the rolltime specified above, but the roll time will vary based on when the random target is found
-  //The function will continue while the specified minroll and maxroll have not been reached, checked by comparing to currentroll
-  while (
-    Math.floor(Math.random() * avgRolls + 1) !== avgRolls ||
-    currentRoll < minRolls ||
-    currentRoll === maxrolls
-  ) {
-    let random = Math.floor(Math.random() * diceValue + 1) + modifier;
+  document.getElementById("rolls-array").innerHTML = outputString;
+
+  let rolls = rolltimes[diceValue];
+
+  //roll until specified rolls are completed, values given by the user inputted slider value and the specific dice
+  while (currentRoll !== Math.floor((rolls * rolltimeValueFromSlider) ^ 1.5)) {
+    var random = Math.floor(Math.random() * diceValue + 1); //random is a var so it can be used outside the scope of the loop
 
     if (diceValue == 2 && random == 1) {
       //Could have used ternary, but would be too bulky. This is the coinflipping logic
@@ -61,17 +62,32 @@ async function rollDice(diceValue, diceId) {
     } else if (diceValue == 2 && random == 2) {
       document.getElementById("display").innerHTML = coinFaces[1];
     } else {
-      document.getElementById("display").innerHTML = random;
+      document.getElementById("display").innerHTML =
+        modifier != 0 ? random + "+" + modifier : random; //no need to display "+0" so I just emit that part if modifier = 0
     }
-    await delay(15);
+    await delay(calculateDelayReg(currentRoll));
 
     currentRoll++;
   }
+
+  var final = random + modifier;
+  for (let i = 0; i < 3; i++) {
+    document.getElementById("display").innerHTML = final;
+    await delay(500);
+    document.getElementById("display").innerHTML = "";
+    await delay(500);
+  }
+
+  document.getElementById("display").innerHTML = final;
+  storeRoll(final, diceId, modifier);
 }
 
-function calculateDelay(diceValue, currentTime) {
+function calculateDelayReg(currentRoll) {
+  return 30;
   //Todo: calculate the delay based on oscillating decay function,
 }
+
+function calculateDelayExponentialDecay(currentRoll) {}
 
 async function attributeGenerator() {
   /**
@@ -81,13 +97,9 @@ async function attributeGenerator() {
    */
 }
 
-function storeRoll(roll) {
-  document.getElementById("rolls-array").innerHTML = roll;
-
-  /** 
-  rollsArray.push[roll];
-  console.log(rollsArray);
-  document.getElementById("rolls-array").innerHTML = rollsArray.join("\n");
-  console.log(roll);
-  */
+function storeRoll(roll, diceID, modifier) {
+  let filteredDiceID = diceID.replace(/[^0-9]/g, ''); //Filter just the number out of the id value
+  var outputString = //format the output string
+    "D" + filteredDiceID.toString() + " + " + modifier.toString() + ": " + roll.toString();
+  document.getElementById("rolls-array").innerHTML = outputString; //display on the scroll
 }
